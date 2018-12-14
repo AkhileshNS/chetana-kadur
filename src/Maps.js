7bf77beeebe2dfdf7a2854c3a94ec9dca81fcdfb
AkhileshNS <nsakhilesh02@gmail.com>
2018-12-14 20:16:45 +0530

    Changed Location

--- src/Maps.js
+++ src/Maps.js
@@ -5,8 +5,8 @@ import {GoogleMap, Marker, withGoogleMap, withScriptjs} from 'react-google-maps'
 const Maps = withScriptjs(withGoogleMap((props) => (
     <GoogleMap
         defaultZoom={15}
-        defaultCenter={{ lat: 13.5525741, lng: 76.0101872 }}>
-        {props.isMarkerShown && <Marker title="Chetana Specialty Hospital" position={{ lat: 13.5525741, lng: 76.0101872 }} />}
+        defaultCenter={{ lat: 13.5464562, lng: 76.0187962 }}>
+        {props.isMarkerShown && <Marker title="Chetana Specialty Hospital" position={{ lat: 13.5464562, lng: 76.0187962 }} />}
     </GoogleMap>
 )));
 
